8ca6f1de1a67c728e250b41614cb84d78d8e73c9
quynt55 <nguyenquy.cnk55@gmail.com>
2013-09-25 16:00:10 +0700

    abc

--- triangle/test.py
+++ triangle/test.py
@@ -22,32 +22,42 @@ class Test(unittest.TestCase):
     def testName6(self):
         self.assertEqual(detect_triangle(5,6,"a"), "Nhap sai")
     def testName7(self):
-        self.assertEqual(detect_triangle(1,1,3), "Khong phai la ba canh tam giac!")
+        self.assertEqual(detect_triangle(2**32,2**32-1,4), "Nhap sai")
     def testName8(self):
-        self.assertEqual(detect_triangle(1,3,1), "Khong phai la ba canh tam giac!")
+        self.assertEqual(detect_triangle(2**32-1,2**32,4), "Nhap sai")
     def testName9(self):
-        self.assertEqual(detect_triangle(3,1,1), "Khong phai la ba canh tam giac!")
+        self.assertEqual(detect_triangle(4,2**32-1,2**32), "Nhap sai")
     def testName10(self):
-        self.assertEqual(detect_triangle(3,3,3), "Tam giac deu")
+        self.assertEqual(detect_triangle(1,1,3), "Khong phai la ba canh tam giac!")
     def testName11(self):
-        self.assertEqual(detect_triangle(3,2,3), "Tam giac can")
+        self.assertEqual(detect_triangle(1,3,1), "Khong phai la ba canh tam giac!")
     def testName12(self):
-        self.assertEqual(detect_triangle(3,3,2), "Tam giac can")
+        self.assertEqual(detect_triangle(3,1,1), "Khong phai la ba canh tam giac!")
     def testName13(self):
-        self.assertEqual(detect_triangle(2,3,3), "Tam giac can")
+        self.assertEqual(detect_triangle(3,3,3), "Tam giac deu")
+        self.assertEqual(detect_triangle(2**32-1,2**32-1,2**32-1), "Tam giac deu")
     def testName14(self):
-        self.assertEqual(detect_triangle(3,4,5), "Tam giac vuong")
+        self.assertEqual(detect_triangle(3,2,3), "Tam giac can")
+        self.assertEqual(detect_triangle(2**32-1,4,2**32-1), "Tam giac can")
     def testName15(self):
-        self.assertEqual(detect_triangle(3,5,4), "Tam giac vuong")
+        self.assertEqual(detect_triangle(3,3,2), "Tam giac can")
+        self.assertEqual(detect_triangle(2**32-1,2**32-1,4), "Tam giac can")
     def testName16(self):
-        self.assertEqual(detect_triangle(5,4,3), "Tam giac vuong")
+        self.assertEqual(detect_triangle(2,3,3), "Tam giac can")
+        self.assertEqual(detect_triangle(4,2**32-1,2**32-1), "Tam giac can")
     def testName17(self):
-        self.assertEqual(detect_triangle(1,1,math.sqrt(2)), "Tam giac vuong can")
+        self.assertEqual(detect_triangle(3,4,5), "Tam giac vuong")
     def testName18(self):
-        self.assertEqual(detect_triangle(1,math.sqrt(2),1), "Tam giac vuong can")
+        self.assertEqual(detect_triangle(3,5,4), "Tam giac vuong")
     def testName19(self):
-        self.assertEqual(detect_triangle(math.sqrt(2),1,1), "Tam giac vuong can")
+        self.assertEqual(detect_triangle(5,4,3), "Tam giac vuong")
     def testName20(self):
+        self.assertEqual(detect_triangle(1,1,math.sqrt(2)), "Tam giac vuong can")
+    def testName21(self):
+        self.assertEqual(detect_triangle(1,math.sqrt(2),1), "Tam giac vuong can")
+    def testName22(self):
+        self.assertEqual(detect_triangle(math.sqrt(2),1,1), "Tam giac vuong can")
+    def testName23(self):
         self.assertEqual(detect_triangle(3,5,6), "Tam giac thuong")
     
 
--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,6 @@
+import math
 def checkInput(a,b,c):
-    if((type(a) in [int, float, long]) and(type(b) in [int, float, long]) and (type(c) in [int, float, long]) and (a>0) and (b>0) and (c>0) ):
+    if((type(a) in [int, float, long]) and (type(b) in [int, float, long]) and (type(c) in [int, float, long]) and (a>0 and a <= 2**32-1) and (b>0 and b <= 2**32-1) and (c>0 and c <= 2**32-1) ):
         return 1
     else:
         return 0
@@ -7,16 +8,16 @@ def detect_triangle(a,b,c):
     if(checkInput(a,b,c) == 0 ):
         return "Nhap sai"
     else:
-        if( (a + b) <= c)or((a+c)<=b)or((b+c)<=a):
+        if((a+b)<=c) or ((a+c)<=b) or ((b+c)<=a):
             return "Khong phai la ba canh tam giac!"
-        elif((a==b)and(b==c)and(a==c)):
+        elif((a==b) and (b==c) and (a==c)):
             return "Tam giac deu"
-        elif((a==b)or(a==c)or(b==c)):
-            if(((a*a+b*b)-c*c) <= 10**-9 or((a*a + c*c)-b*b)<= 10**-9 or((b*b + c*c) - a*a)<=10**-9):
+        elif((a==b) or (a==c) or (b==c)):
+            if((math.fabs(a*a-b*b-c*c) <= 10**-9 and b==c) or (math.fabs(c*c - a*a-b*b)<= 10**-9 and a==b) or (math.fabs(b*b - c*c - a*a)<=10**-9 and a==c)):
                 return "Tam giac vuong can"
             else:
                 return "Tam giac can"
-        elif(((a*a+b*b)==c*c)or((a*a + c*c)==b*b)or((b*b + c*c) == a*a)):
+        elif((math.fabs(a*a-b*b-c*c) <= 10**-9) or (math.fabs(c*c - a*a-b*b)<= 10**-9) or (math.fabs(b*b - c*c - a*a)<=10**-9)):
             return "Tam giac vuong"
         else:
             return "Tam giac thuong"
